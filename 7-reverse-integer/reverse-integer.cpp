99702eaae0df3ec8635c70c3651d2d01911b38da
MansiK607 <2023ugee032@nitjsr.ac.in>
2025-05-26 18:10:23 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (53.87%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     int reverse(int x) {
         int revNum=0;
-        if(pow(2,-31)>=x>=(pow(2,31)+1)){
-            return 0;
-        }
+       
         while(x!=0){
             int ld=x%10;
             
