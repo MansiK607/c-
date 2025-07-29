3aac82509a0881df3622659e756e5167254db29f
MansiK607 <2023ugee032@nitjsr.ac.in>
2025-07-30 01:24:38 +0530

    Time: 10 ms (46.81%) | Memory: 11.4 MB (54.03%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -9,13 +9,20 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {      
-        ListNode*temp=head;
-        map<ListNode*,int> mpp;
-        while(temp!=NULL){
-            if(mpp.find(temp)!=mpp.end()) return temp;
-            mpp[temp]=1;
-            temp=temp->next;
-        }
-        return NULL;
+        ListNode*slow=head;
+        ListNode*fast=head;
+          while(fast!=NULL && fast->next!=NULL){
+       slow=slow->next;
+       fast=fast->next->next;
+       if(slow==fast){
+        slow=head;
+           while(slow!=fast){
+               slow=slow->next;
+               fast=fast->next;
+           }
+           return fast;
+       }
+   }
+   return NULL;
     }
 };
